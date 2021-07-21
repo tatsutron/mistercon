4c250769d5dab78b803d346d5f1e428b3aa3f36f
tatsutron <51144928+tatsutron@users.noreply.github.com>
2021-07-20 20:33:12 -0500

    Add constants

---
+++ app/src/main/java/com/tatsutron/remote/Constants.kt
@@ -0,0 +1,9 @@
+package com.tatsutron.remote
+
+object Constants {
+    const val GAMES_PATH = "/media/fat/games"
+    const val HOST = "MiSTer"
+    const val MBC_PATH = "/media/fat/mistercon/mbc"
+    const val SCAN_PATH = "/media/fat/mistercon/scan"
+    const val SCRIPTS_PATH = "/media/fat/_Scripts"
+}
