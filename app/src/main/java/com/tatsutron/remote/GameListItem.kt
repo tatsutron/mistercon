cabde31c55051d444722faa015e1cacf1d80a6ec
tatsutron <51144928+tatsutron@users.noreply.github.com>
2021-07-20 20:14:37 -0500

    Delete a bunch of soon-to-be-unused files
    Note that for the next several commits, including this one, the app will
    be in a partially broken state.

--- app/src/main/java/com/tatsutron/remote/GameListItem.kt
+++
@@ -1,9 +0,0 @@
-package com.tatsutron.remote
-
-class GameListItem(
-    var isExpanded: Boolean = false,
-    val label: String,
-    val labelColor: Int? = null,
-    var onClick: (() -> Unit)? = null,
-    val parent: GameListItem? = null,
-)
