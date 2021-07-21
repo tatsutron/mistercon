1834d60c0cf5a92c8df1fc51e9524718e63e4ea4
tatsutron <51144928+tatsutron@users.noreply.github.com>
2021-07-20 20:25:10 -0500

    Update system fragment ui

--- app/src/main/java/com/tatsutron/remote/SystemFragment.kt
+++ app/src/main/java/com/tatsutron/remote/SystemFragment.kt
@@ -7,15 +7,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.EditText
-import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.textfield.TextInputEditText
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
-import java.io.File
 
 class SystemFragment : Fragment(), CoroutineScope by MainScope() {
 
@@ -31,67 +28,38 @@ class SystemFragment : Fragment(), CoroutineScope by MainScope() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setHost(view)
-        setGamesPath(view)
         setMenuButton(view)
         setScriptButton(view)
         setRebootButton(view)
-        Event.SYNC.subscribe {
-            setScriptButton(view)
-        }
     }
 
     private fun setHost(view: View) {
-        val card = view.findViewById<EditCard>(R.id.host)
-        val value = card.findViewById<EditText>(R.id.value)
-        value.setText(Persistence.getHost())
-        value.setSelection(value.text.length)
-        value.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(
-                s: CharSequence?,
-                start: Int,
-                count: Int,
-                after: Int,
-            ) {
+        view.findViewById<TextInputEditText>(R.id.host_text).apply {
+            setText(Persistence.getConfig()?.host)
+            text?.let {
+                setSelection(it.length)
             }
+            addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    count: Int,
+                    after: Int,
+                ) {
+                }
 
-            override fun onTextChanged(
-                s: CharSequence?,
-                start: Int,
-                before: Int,
-                count: Int,
-            ) {
-                Persistence.saveHost(s.toString())
-            }
+                override fun onTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    before: Int,
+                    count: Int,
+                ) {
+                    Persistence.saveHost(s.toString())
+                }
 
-            override fun afterTextChanged(s: Editable?) {}
-        })
-    }
-
-    private fun setGamesPath(view: View) {
-        val card = view.findViewById<EditCard>(R.id.games_path)
-        val value = card.findViewById<EditText>(R.id.value)
-        value.setText(Persistence.getGamesPath())
-        value.setSelection(value.text.length)
-        value.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(
-                s: CharSequence?,
-                start: Int,
-                count: Int,
-                after: Int,
-            ) {
-            }
-
-            override fun onTextChanged(
-                s: CharSequence?,
-                start: Int,
-                before: Int,
-                count: Int,
-            ) {
-                Persistence.saveGamesPath(s.toString())
-            }
-
-            override fun afterTextChanged(s: Editable?) {}
-        })
+                override fun afterTextChanged(s: Editable?) {}
+            })
+        }
     }
 
     private fun setMenuButton(view: View) {
@@ -100,8 +68,7 @@ class SystemFragment : Fragment(), CoroutineScope by MainScope() {
             launch(Dispatchers.IO) {
                 runCatching {
                     val session = Ssh.session()
-                    val mbc = Persistence.getMbcPath()
-                    Ssh.command(session, "$mbc raw_seq M")
+                    Ssh.command(session, "${Constants.MBC_PATH} raw_seq M")
                     session.disconnect()
                 }
             }
@@ -109,43 +76,37 @@ class SystemFragment : Fragment(), CoroutineScope by MainScope() {
     }
 
     private fun setScriptButton(view: View) {
-        val scripts = Persistence.getScriptList()
-        val scriptButton = view.findViewById<Button>(R.id.script_button)
-        scriptButton.visibility = if (scripts.isNotEmpty()) {
-            View.VISIBLE
-        } else {
-            View.GONE
-        }
-        scriptButton.setOnClickListener {
-            val context = requireContext()
-            MaterialAlertDialogBuilder(context)
-                .setTitle(context.getString(R.string.script))
-                .setNegativeButton(
-                    context.getString(R.string.cancel),
-                    null, // listener
-                )
-                .setPositiveButton(
-                    context.getString(R.string.run)
-                ) { dialog, _ ->
-                    val list = (dialog as AlertDialog).listView
-                    val script = list.adapter.getItem(list.checkedItemPosition)
-                            as? String
-                    val path = File(Persistence.getScriptsPath(), script).path
-                    launch(Dispatchers.IO) {
-                        runCatching {
-                            val session = Ssh.session()
-                            Scripts.run(session, path)
-                            session.disconnect()
-                        }
-                    }
-                }
-                .setSingleChoiceItems(
-                    Persistence.getScriptList().toTypedArray(),
-                    0, // checkedItem
-                    null, // listener
-                )
-                .show()
-        }
+//        val scriptButton = view.findViewById<Button>(R.id.script_button)
+//        scriptButton.setOnClickListener {
+//            val context = requireContext()
+//            MaterialAlertDialogBuilder(context)
+//                .setTitle(context.getString(R.string.script))
+//                .setNegativeButton(
+//                    context.getString(R.string.cancel),
+//                    null, // listener
+//                )
+//                .setPositiveButton(
+//                    context.getString(R.string.run)
+//                ) { dialog, _ ->
+//                    val list = (dialog as AlertDialog).listView
+//                    val script = list.adapter.getItem(list.checkedItemPosition)
+//                            as? String
+//                    val path = File(Constants.SCRIPTS_PATH, script).path
+//                    launch(Dispatchers.IO) {
+//                        runCatching {
+//                            val session = Ssh.session()
+//                            Scripts.run(session, path)
+//                            session.disconnect()
+//                        }
+//                    }
+//                }
+//                .setSingleChoiceItems(
+//                    Persistence.getScriptList().toTypedArray(),
+//                    0, // checkedItem
+//                    null, // listener
+//                )
+//                .show()
+//        }
     }
 
     private fun setRebootButton(view: View) {
