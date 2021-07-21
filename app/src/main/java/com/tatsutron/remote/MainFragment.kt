fd550fb175231343b3c85abd39db1377755431b2
tatsutron <51144928+tatsutron@users.noreply.github.com>
2021-07-20 20:21:43 -0500

    Remove sync functionality from main fragment
    This will now be done for individual cores rather than all at once.

--- app/src/main/java/com/tatsutron/remote/MainFragment.kt
+++ app/src/main/java/com/tatsutron/remote/MainFragment.kt
@@ -1,23 +1,13 @@
 package com.tatsutron.remote
 
-import android.content.DialogInterface
 import android.os.Bundle
 import android.view.*
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.viewpager.widget.ViewPager
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
-import com.jcraft.jsch.Session
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.MainScope
-import kotlinx.coroutines.launch
-import java.io.File
-import java.io.FileOutputStream
 
-class MainFragment : Fragment(), CoroutineScope by MainScope() {
+class MainFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,29 +27,13 @@ class MainFragment : Fragment(), CoroutineScope by MainScope() {
         return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.sync -> {
-                val context = requireContext()
-                item.isEnabled = false
-                item.icon.setTint(context.getColor(R.color.gray_800))
-                sync {
-                    item.icon.setTint(context.getColor(R.color.white))
-                    item.isEnabled = true
-                }
-                true
-            }
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val viewPager = view.findViewById<ViewPager>(R.id.view_pager)
         val bottomNavigation = view
             .findViewById<BottomNavigationView>(R.id.bottom_navigation)
         (activity as? AppCompatActivity)?.apply {
-            setSupportActionBar(view.findViewById(R.id.main_toolbar))
+            setSupportActionBar(view.findViewById(R.id.toolbar))
             supportActionBar?.title = bottomNavigation.menu.getItem(0).title
         }
         viewPager.adapter = FragmentPagerAdapter(fragmentManager!!)
@@ -78,7 +52,7 @@ class MainFragment : Fragment(), CoroutineScope by MainScope() {
                     bottomNavigation.selectedItemId = item.itemId
                     (activity as? AppCompatActivity)?.apply {
                         setSupportActionBar(
-                            view.findViewById(R.id.main_toolbar)
+                            view.findViewById(R.id.toolbar)
                         )
                         supportActionBar?.title = item.title
                     }
@@ -91,75 +65,13 @@ class MainFragment : Fragment(), CoroutineScope by MainScope() {
                     viewPager.currentItem = i
                     (activity as? AppCompatActivity)?.apply {
                         setSupportActionBar(
-                            view.findViewById(R.id.main_toolbar)
+                            view.findViewById(R.id.toolbar)
                         )
                         supportActionBar?.title = item.title
                     }
                 }
             }
             true
-        }
-    }
-
-    private fun sync(cb: () -> Unit) {
-        Persistence.clearGames()
-        Persistence.clearScripts()
-        Event.SYNC.fire()
-        launch(Dispatchers.IO) {
-            runCatching {
-                val session = Ssh.session()
-                install(session)
-                Core.values().forEach {
-                    it.sync(session)
-                }
-                Scripts.sync(session)
-                session.disconnect()
-            }.onSuccess {
-                requireActivity().runOnUiThread {
-                    cb()
-                    Event.SYNC.fire()
-                }
-            }.onFailure {
-                val context = requireContext()
-                val ok = { _: DialogInterface, _: Int ->
-                    cb()
-                }
-                requireActivity().runOnUiThread {
-                    MaterialAlertDialogBuilder(context)
-                        .setTitle(context.getString(R.string.sync_error))
-                        .setMessage(it.toString())
-                        .setPositiveButton(context.getString(R.string.ok), ok)
-                        .show()
-                }
-            }
-        }
-    }
-
-    private fun install(session: Session) {
-        Ssh.sftp(session).apply {
-            try {
-                mkdir("/media/fat/mistercon")
-            } catch (exception: Throwable) {
-
-            }
-            disconnect()
-        }
-        install(session, "mbc")
-    }
-
-    private fun install(session: Session, name: String) {
-        val context = requireContext()
-        val file = File(File(context.cacheDir, name).path)
-        val input = requireContext().assets.open(name)
-        val buffer = input.readBytes()
-        input.close()
-        FileOutputStream(file).apply {
-            write(buffer)
-            close()
-        }
-        Ssh.sftp(session).apply {
-            put(file.path, File("/media/fat/mistercon", name).path)
-            disconnect()
         }
     }
 }
