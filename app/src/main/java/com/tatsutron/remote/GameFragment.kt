1fca24499cca5dcbbd7ac1f6c638a670086b7a75
tatsutron <51144928+tatsutron@users.noreply.github.com>
2021-07-20 20:21:03 -0500

    Update game fragment ui

--- app/src/main/java/com/tatsutron/remote/GameFragment.kt
+++ app/src/main/java/com/tatsutron/remote/GameFragment.kt
@@ -1,9 +1,16 @@
 package com.tatsutron.remote
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.*
+import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import com.bumptech.glide.Glide
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.MainScope
@@ -11,6 +18,7 @@ import kotlinx.coroutines.launch
 import java.io.File
 
 class GameFragment : Fragment(), CoroutineScope by MainScope() {
+    private lateinit var game: Game
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,12 +43,20 @@ class GameFragment : Fragment(), CoroutineScope by MainScope() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.play -> {
-                val id = arguments?.getLong(FragmentMaker.KEY_ID)
-                val game = Persistence.getGame(id!!)!!
                 launch(Dispatchers.IO) {
                     runCatching {
                         val session = Ssh.session()
-                        game.core.play(session, game)
+                        val extension = File(game.path).extension
+                        val command = StringBuilder().apply {
+                            append("\"${Constants.MBC_PATH}\"")
+                            append(" ")
+                            append("load_rom")
+                            append(" ")
+                            append(game.core.commandsByExtension[extension])
+                            append(" ")
+                            append("\"${game.path}\"")
+                        }.toString()
+                        Ssh.command(session, command)
                         session.disconnect()
                     }
                 }
@@ -52,42 +68,100 @@ class GameFragment : Fragment(), CoroutineScope by MainScope() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val id = arguments?.getLong(FragmentMaker.KEY_ID)
-        val game = Persistence.getGame(id!!)!!
+        game = Persistence.getGameById(
+            arguments?.getLong(FragmentMaker.KEY_ID)!!
+        )!!
         (activity as? AppCompatActivity)?.apply {
             setSupportActionBar(view.findViewById(R.id.game_toolbar))
             supportActionBar?.title = game.release?.releaseTitleName
                 ?: File(game.path).name
         }
-        view.findViewById<TextCard>(R.id.publisher).set(
-            game.release?.releasePublisher
-        )
-        view.findViewById<TextCard>(R.id.developer).set(
-            game.release?.releaseDeveloper
-        )
-        view.findViewById<TextCard>(R.id.release_date).set(
-            game.release?.releaseDate
-        )
-        view.findViewById<TextCard>(R.id.region).set(
-            game.region?.regionName
-        )
-        view.findViewById<ImageCard>(R.id.front_cover).set(
-            imageUrl = game.release?.releaseCoverFront,
-            activity = activity,
-        )
-        view.findViewById<ImageCard>(R.id.back_cover).set(
-            imageUrl = game.release?.releaseCoverBack,
-            activity = activity,
-        )
-        view.findViewById<ImageCard>(R.id.cartridge).set(
-            imageUrl = game.release?.releaseCoverCart,
-            activity = activity,
-        )
-        view.findViewById<TextCard>(R.id.genre).set(
-            game.release?.releaseGenre
-        )
-        view.findViewById<TextCard>(R.id.description).set(
-            game.release?.releaseDescription
-        )
+        game.release?.releasePublisher?.let {
+            view.findViewById<TextInputEditText>(R.id.publisher_text)
+                .setText(it)
+            view.findViewById<TextInputLayout>(R.id.publisher_layout)
+                .visibility = View.VISIBLE
+        }
+        game.release?.releaseDeveloper?.let {
+            view.findViewById<TextInputEditText>(R.id.developer_text)
+                .setText(it)
+            view.findViewById<TextInputLayout>(R.id.developer_layout)
+                .visibility = View.VISIBLE
+        }
+        game.release?.releaseDate?.let {
+            view.findViewById<TextInputEditText>(R.id.release_date_text)
+                .setText(it)
+            view.findViewById<TextInputLayout>(R.id.release_date_layout)
+                .visibility = View.VISIBLE
+        }
+        game.region?.regionName?.let {
+            view.findViewById<TextInputEditText>(R.id.region_text)
+                .setText(it)
+            view.findViewById<TextInputLayout>(R.id.region_layout)
+                .visibility = View.VISIBLE
+        }
+        game.release?.releaseGenre?.let {
+            view.findViewById<TextInputEditText>(R.id.genre_text)
+                .setText(it)
+            view.findViewById<TextInputLayout>(R.id.genre_layout)
+                .visibility = View.VISIBLE
+        }
+        game.release?.releaseDescription?.let {
+            view.findViewById<TextInputEditText>(R.id.description_text)
+                .setText(it)
+            view.findViewById<TextInputLayout>(R.id.description_layout)
+                .visibility = View.VISIBLE
+        }
+        game.release?.releaseCoverFront?.let { url ->
+            view.findViewById<ImageView>(R.id.front_cover_image).apply {
+                Glide.with(this@GameFragment)
+                    .load(Uri.parse(url))
+                    .into(this)
+                setOnClickListener {
+                    activity?.supportFragmentManager
+                        ?.beginTransaction()
+                        ?.setTransition(FragmentTransaction.TRANSIT_NONE)
+                        ?.add(R.id.root, FragmentMaker.image(url))
+                        ?.addToBackStack(null)
+                        ?.commit()
+                }
+            }
+            view.findViewById<LinearLayout>(R.id.front_cover_layout)
+                .visibility = View.VISIBLE
+        }
+        game.release?.releaseCoverBack?.let { url ->
+            view.findViewById<ImageView>(R.id.back_cover_image).apply {
+                Glide.with(this@GameFragment)
+                    .load(Uri.parse(url))
+                    .into(this)
+                setOnClickListener {
+                    activity?.supportFragmentManager
+                        ?.beginTransaction()
+                        ?.setTransition(FragmentTransaction.TRANSIT_NONE)
+                        ?.add(R.id.root, FragmentMaker.image(url))
+                        ?.addToBackStack(null)
+                        ?.commit()
+                }
+            }
+            view.findViewById<LinearLayout>(R.id.back_cover_layout)
+                .visibility = View.VISIBLE
+        }
+        game.release?.releaseCoverCart?.let { url ->
+            view.findViewById<ImageView>(R.id.cartridge_image).apply {
+                Glide.with(this@GameFragment)
+                    .load(Uri.parse(url))
+                    .into(this)
+                setOnClickListener {
+                    activity?.supportFragmentManager
+                        ?.beginTransaction()
+                        ?.setTransition(FragmentTransaction.TRANSIT_NONE)
+                        ?.add(R.id.root, FragmentMaker.image(url))
+                        ?.addToBackStack(null)
+                        ?.commit()
+                }
+            }
+            view.findViewById<LinearLayout>(R.id.cartridge_layout)
+                .visibility = View.VISIBLE
+        }
     }
 }
