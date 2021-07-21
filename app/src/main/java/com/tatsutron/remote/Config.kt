1cd895f85ad28f91fa9ffb7e26a0eab17da76781
tatsutron <51144928+tatsutron@users.noreply.github.com>
2021-07-20 20:17:50 -0500

    Drop gamesPath and mbcPath from config
    Games path will now be stored per-core, and mbc path will never change.

--- app/src/main/java/com/tatsutron/remote/Config.kt
+++ app/src/main/java/com/tatsutron/remote/Config.kt
@@ -1,8 +1,6 @@
 package com.tatsutron.remote
 
 class Config(
-    var gamesPath: String = "/media/fat/games",
-    var host: String = "MiSTer",
-    var mbcPath: String = "/media/fat/mistercon/mbc",
-    var scriptsPath: String = "/media/fat/_Scripts",
+    val host: String,
+    val scriptsPath: String,
 )
