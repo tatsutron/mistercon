956f1996b3ec023be10ce7ab0d0126903a2b6491
tatsutron <51144928+tatsutron@users.noreply.github.com>
2021-07-21 19:38:56 -0500

    Add mistercon path constant

--- app/src/main/java/com/tatsutron/remote/Constants.kt
+++ app/src/main/java/com/tatsutron/remote/Constants.kt
@@ -4,6 +4,7 @@ object Constants {
     const val GAMES_PATH = "/media/fat/games"
     const val HOST = "MiSTer"
     const val MBC_PATH = "/media/fat/mistercon/mbc"
+    const val MISTERCON_PATH = "/media/fat/mistercon"
     const val SCAN_PATH = "/media/fat/mistercon/scan"
     const val SCRIPTS_PATH = "/media/fat/_Scripts"
 }
