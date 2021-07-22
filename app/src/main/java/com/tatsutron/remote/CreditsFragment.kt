1800c1022aa9f2d019b95dd2a0ea64c1492eeb88
tatsutron <51144928+tatsutron@users.noreply.github.com>
2021-07-21 21:20:42 -0500

    Populate credits screen

--- app/src/main/java/com/tatsutron/remote/CreditsFragment.kt
+++ app/src/main/java/com/tatsutron/remote/CreditsFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 
 class CreditsFragment : Fragment() {
@@ -23,5 +25,17 @@ class CreditsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        view.findViewById<LinearLayout>(R.id.credits_container).apply {
+            listOf(
+                "DoOoM",
+                "KremlingKuthroat19",
+                "Luke Earnshaw",
+                "Mist Sonata",
+            ).forEach {
+                val item = layoutInflater.inflate(R.layout.item_credit, null)
+                item.findViewById<TextView>(R.id.label).text = it
+                addView(item)
+            }
+        }
     }
 }
