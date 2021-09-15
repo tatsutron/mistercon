12782ac4dfa60a63781ed6285646689fd9569855
tatsutron <51144928+tatsutron@users.noreply.github.com>
2021-09-14 20:10:46 -0500

    Use material-dialogs wherever applicable

--- android/app/src/main/kotlin/com/tatsutron/remote/Dialog.kt
+++ android/app/src/main/kotlin/com/tatsutron/remote/Dialog.kt
@@ -2,61 +2,41 @@ package com.tatsutron.remote
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.content.DialogInterface
 import android.text.InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS
+import com.afollestad.materialdialogs.DialogCallback
 import com.afollestad.materialdialogs.MaterialDialog
 import com.afollestad.materialdialogs.input.InputCallback
 import com.afollestad.materialdialogs.input.input
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 object Dialog {
 
     fun confirm(
         context: Context,
         message: String,
-        cancel: () -> Unit = {},
-        ok: () -> Unit = {},
-    ) {
-        MaterialAlertDialogBuilder(context, R.style.Dialog)
-            .setMessage(message)
-            .setNegativeButton(context.getString(R.string.cancel))
-            { _: DialogInterface, _: Int ->
-                cancel()
-            }
-            .setPositiveButton(context.getString(R.string.ok))
-            { _: DialogInterface, _: Int ->
-                ok()
-            }
-            .show()
+        cancel: DialogCallback? = null,
+        ok: DialogCallback,
+    ) = MaterialDialog(context).show {
+        message(text = message)
+        negativeButton(res = R.string.cancel, click = cancel)
+        positiveButton(res = R.string.ok, click = ok)
     }
 
     fun error(
         context: Context,
         throwable: Throwable,
-        ok: () -> Unit = {},
-    ) {
-        MaterialAlertDialogBuilder(context, R.style.Dialog)
-            .setTitle(context.getString(R.string.error))
-            .setMessage(throwable.toString())
-            .setPositiveButton(context.getString(R.string.ok))
-            { _: DialogInterface, _: Int ->
-                ok()
-            }
-            .show()
+    ) = MaterialDialog(context).show {
+        title(R.string.error)
+        message(text = throwable.toString())
+        positiveButton(R.string.ok)
     }
 
     fun info(
         context: Context,
         message: String,
-        ok: () -> Unit = {},
-    ) {
-        MaterialAlertDialogBuilder(context, R.style.Dialog)
-            .setMessage(message)
-            .setPositiveButton(context.getString(R.string.ok))
-            { _: DialogInterface, _: Int ->
-                ok()
-            }
-            .show()
+        ok: DialogCallback,
+    ) = MaterialDialog(context).show {
+        message(text = message)
+        positiveButton(res = R.string.ok, click = ok)
     }
 
     @SuppressLint("CheckResult")
