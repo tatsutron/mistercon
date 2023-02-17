3bdb9ac39a047b1856c0784dc6fabbf789d4f7fd
tatsutron <51144928+tatsutron@users.noreply.github.com>
2023-02-17 06:23:46 -0600

    Revert "Nest mister_util.scan"
    This reverts commit fd7b0fe8835ef27288f1cb1e08ab28a4e0c6868d.

--- android/app/src/main/assets/mister_util.py
+++ android/app/src/main/assets/mister_util.py
@@ -1,21 +1,23 @@
 import hashlib, os, sys
 
 
+def scan(path, extensions, entries):
+    with os.scandir(path) as it:
+        for entry in it:
+            root, extension = os.path.splitext(entry.path)
+            if os.path.basename(root).startswith("."):
+                pass
+            elif entry.is_dir():
+                scan(entry, extensions, entries)
+            else:
+                if extension[1:] in extensions.split("|"):
+                    entries.append(entry.path)
+
+
 if sys.argv[1] == "scan":
     path, extensions = sys.argv[2:]
     entries = []
-    def scan():
-        with os.scandir(path) as it:
-            for entry in it:
-                root, extension = os.path.splitext(entry.path)
-                if os.path.basename(root).startswith("."):
-                    pass
-                elif entry.is_dir():
-                    scan()
-                else:
-                    if extension[1:] in extensions.split("|"):
-                        entries.append(entry.path)
-    scan()
+    scan(path, extensions, entries)
     print(";".join(entries), end="")
 
 
