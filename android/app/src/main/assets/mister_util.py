cd2341c9540041142f5bd036f430229c78273917
tatsutron <51144928+tatsutron@users.noreply.github.com>
2023-02-24 12:47:29 -0600

    Add zip file support to mister_util.scan

--- android/app/src/main/assets/mister_util.py
+++ android/app/src/main/assets/mister_util.py
@@ -1,4 +1,13 @@
-import hashlib, os, sys
+import hashlib, os, sys, zipfile
+
+
+def scanzip(path, extensions, entries):
+    archive = zipfile.ZipFile(path)
+    blocksize = 1024**2
+    for entry in archive.namelist():
+        root, extension = os.path.splitext(entry)
+        if extension and extension[1:] in extensions.split("|"):
+            entries.append(os.path.join(path, entry))
 
 
 def scan(path, extensions, entries):
@@ -8,10 +17,12 @@ def scan(path, extensions, entries):
             if os.path.basename(root).startswith("."):
                 pass
             elif entry.is_dir():
-                scan(entry, extensions, entries)
+                scan(entry.path, extensions, entries)
             else:
                 if extension[1:] in extensions.split("|"):
                     entries.append(entry.path)
+                elif extension[1:] == "zip":
+                    scanzip(entry.path, extensions, entries)
 
 
 if sys.argv[1] == "scan":
